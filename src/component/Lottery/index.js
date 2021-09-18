baea62e6ee9e8409d2e39b9f5fcca3f389382229
Sukie-chen <csq1410@163.com>
2021-09-18 11:40:30 +0800

    获取服务器中随机抽取的奖品id

--- src/component/Lottery/index.js
+++ src/component/Lottery/index.js
@@ -1,20 +1,35 @@
-import React, {useRef, useState} from "react";
+/*
+ * @Author: Sukie-Chen
+ * @Description: 
+ * @Date: 2021-08-26 18:15:56
+ */
+import React, {useEffect, useRef, useState} from "react";
 import './lottery.css'
 import {Win} from "../Win";
 import {store} from '../../Store'
 import axios from "axios";
-axios.get('/api/live').then((res) => {
-    store.dispatch({
-        type: "UPDATE_CATEGORY",
-        value: res.data
-    })
-}).catch((err) => {
-    console.log('数据请求失败', err)
-})
-let {category, startBtn} = store.getState()
+
 export const Lottery = () => {
+    const {category, startBtn} = store.getState()
+    const [, update] = useState({})
     const [active, setActive] = useState(0)
     const [win, setWin] = useState(0)
+    useEffect(() => {
+        axios.get('/api/list').then((res) => {
+            console.log(res.data)
+            store.dispatch({
+                type: "UPDATE_CATEGORY",
+                value: res.data
+            })
+        }).catch((err) => {
+            console.log('数据请求失败', err)
+        })
+        return store.subscribe(()=>{
+            update({})
+        })
+    }, []); //诟病：抽奖过程中样式改变一次就会重新update? 
+    
+
     // 按钮点击
     const handleClick = (key) => {
         if(key===5){
@@ -33,6 +48,7 @@ export const Lottery = () => {
             setActive(idArr[currId])
             if(currId < idLength) {
                 currId++
+                console.log(currId)
             } else {
                 currId = 0 //循环
             }
@@ -45,6 +61,7 @@ export const Lottery = () => {
     const stopLottery = (currId) => {
         //确定是抽中奖品，则点击按钮还原功能 active初始化
         const {award} = store.getState()
+        console.log('award', award)
         if( award!== 5 && currId === award) {
             clearInterval(timer.current)
             setActive(0)
@@ -65,21 +82,27 @@ export const Lottery = () => {
                 value: true
             })
             doingLottery()
-            store.dispatch({
-                type: 'GET_AWARD',
-                value: 7
+            axios.get('/api/getAward').then((res) => {
+                // console.log('id', res.data[0].award_id)
+                store.dispatch({
+                    type: 'GET_AWARD',
+                    value: res.data[0].award_id
+                })
+            }).catch((err) => {
+                console.log('奖品请求失败', err)
             })
+            
         } else {
             console.log('正在抽奖中')
         }
     }
-
+    // console.log(category)
     return <div>
         <ul className="lottery">
-            {category.map((item)=><li className={item.id===5? 'start' : ''} style={{background: active === item.id? '#ffe6a6' : ''}} key={item.id} onClick={()=>handleClick(item.id)}>{item.value}</li>)}
+            {category.map((item)=><li className={item.award_id===5? 'start' : ''} style={{background: active === item.award_id? '#ffe6a6' : ''}} key={item.award_id} onClick={()=>handleClick(item.award_id)}>{item.award_name}</li>)}
         </ul>
         {
-            win === 0? '': <Win currId={win}/>
+            win === 0? '': <Win/>
         }
 
     </div>
--- src/component/Win/index.js
+++ src/component/Win/index.js
@@ -1,6 +1,16 @@
+/*
+ * @Author: Sukie-Chen
+ * @Description: 
+ * @Date: 2021-09-13 16:37:32
+ */
 import React from "react";
 import './win.css'
-export const Win = (props) => {
-    const {currId} = props
-    return <div className="winBox">恭喜抽中奖品{currId}</div>
+import { store } from "../../Store";
+export const Win = () => {
+    const {category, award} = store.getState()
+    const result = category.filter((item => {
+        return item.award_id === award
+    }))
+    // console.log(result)
+    return <div className="winBox">恭喜抽中奖品{result[0].award_name}</div>
 }
